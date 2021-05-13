75c4206d3912ab5668cb308a81087657d9e450ea
Bob den Otter <bob@twokings.nl>
2021-05-13 08:29:58 +0200

    Work

---
+++ src/Controller/Chmod.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Bobdenotter\Chmodinator\Controller;
+
+use Bolt\Extension\ExtensionController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Chmod extends ExtensionController
+{
+    /**
+     * @Route("/chmodinator", name="extension_chmodinator")
+     */
+    public function index(): Response
+    {
+        return new Response('hi');
+    }
+}
